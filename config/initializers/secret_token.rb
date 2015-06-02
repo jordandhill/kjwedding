ca42e55b32f53d83bd5261a64f6e827f33d2c21b
Jordan <jordandhill@gmail.com>
2015-06-01 21:38:07 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kjwedding::Application.config.secret_key_base = '2caa64ae370750ea11bbb2c46f6c2106d4aab68e06ac50871a224a8d34eb697f6b96548dd15697359c694b71a3e02ddb3a953442244aec0263fbef0bbaca0779'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kjwedding::Application.config.session_store :cookie_store, key: '_kjwedding_session'
